b17e9f5c019ba119d19df1c6550a2edc6458fc7a
Harshit Srivastava <harshit67@gmail.com>
2021-10-01 17:55:27 +0530

    Update urls.py

--- inproj/urls.py
+++ inproj/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
+from django.urls import path
 from django.conf.urls import url
 from django.conf.urls import include
 from inpro import views
-from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('inpro.urls')),
-    url(r'^inpro/', include('inpro.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^test_data/$', views.test_data, name='test_data'),
+    url(r'^excel_Download/$', views.excel_Download, name='excel_Download')
 ]
