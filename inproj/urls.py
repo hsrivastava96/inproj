ebda96c835164a96acd6c25631d3bff87a639dd3
uic38334 <harshit.srivastava@continental-corporation.com>
2021-10-01 12:10:41 +0530

    Fourth commit

--- inproj/urls.py
+++ inproj/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.conf.urls import include
 from inpro import views
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include('inpro.urls')),
     url(r'^inpro/', include('inpro.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
